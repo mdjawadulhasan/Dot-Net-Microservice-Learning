3570a42f22b5b184ffef6e7dcdda9fe297b3ef4a
jawadbs23 <66155771+mdjawadulhasan@users.noreply.github.com>
2023-12-15 16:47:17 +0600

    Add project files.

---
+++ Play.Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Play.Catalog.DTOs.DTOs;
+
+namespace Play.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private static List<ItemDto> _items = new List<ItemDto>
+        {
+            new ItemDto(Guid.NewGuid(),"Item 1","Description 1",19.99m,DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(),"Item 2","Description 2",29.99m,DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(),"Item 3","Description 3",39.99m,DateTimeOffset.UtcNow),
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ItemDto>> Get()
+        {
+            return _items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetById(Guid id)
+        {
+            var item = _items.FirstOrDefault(i => i.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Create(CreatedItemDto createItemDto)
+        {
+            var newItem = new ItemDto(Id: Guid.NewGuid(),
+                                      Name: createItemDto.Name,
+                                      Description: createItemDto.Description,
+                                      Price: createItemDto.Price,
+                                      CreatedDate: DateTimeOffset.UtcNow);
+
+            _items.Add(newItem);
+
+            return CreatedAtAction(nameof(GetById), new { id = newItem.Id }, newItem);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(Guid id, UpdatedItemDto updateItemDto)
+        {
+            var existingItem = _items.FirstOrDefault(i => i.Id == id);
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price,
+            };
+
+            var index = _items.FindIndex(x => x.Id == id);
+            _items[index] = updatedItem;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var existingItem = _items.FirstOrDefault(i => i.Id == id);
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            _items.Remove(existingItem);
+
+            return NoContent();
+        }
+    };
+}
+
---
+++ Play.Catalog/DTOs/DTOs.cs
@@ -0,0 +1,10 @@
+﻿namespace Play.Catalog.DTOs
+{
+    public class DTOs
+    {
+        public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+        public record CreatedItemDto(string Name, string Description, decimal Price);
+        public record UpdatedItemDto(string Name, string Description, decimal Price);
+
+    }
+}
